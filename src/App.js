000dd515f216bf91e8411155317f617fd845d091
TemoSulava <sulava.temo@yahoo.com>
2021-04-27 19:38:43 +0400

    Implemented more functions, moved sort method from component to parent

--- src/App.js
+++ src/App.js
@@ -8,42 +8,62 @@ function App() {
   const [questions, setQuestions] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [score, setScore] = useState(0);
-  const [gameDone, setGameDone] = useState(false);
+  const [showCorrect, setShowCorrect] = useState(false);
   
 //fetch the data from the API and set results to questions array
 useEffect(() => {
   axios.get(API_URL)
        .then(res => res.data)
-       .then(data => setQuestions(data.results))
+       .then(data => {
+         const questions = data.results.map(question => 
+          ({
+             ...question,
+             answers: [
+               question.correct_answer,
+               ...question.incorrect_answers,
+             ].sort(() => Math.random() - 0.5)
+           }))
+           setQuestions(questions)
+       });
 }, []);
 
 console.log(questions)
 
   //checking if answer is true or false and acting appropriately
   const handleAnswer = (answer) => {
-    const newIndex = currentIndex + 1
-    setCurrentIndex(newIndex)
 
-    if (answer === questions[currentIndex].correct_answer) {
-      setScore(score + 1)
+    if(!showCorrect) {
+      if (answer === questions[currentIndex].correct_answer) {
+        setScore(score + 1);
+      }
+    
     }
+    setShowCorrect(true)
+  }
 
-    if(newIndex >= questions.length) {
-      setGameDone(true);
-    }
+  const handleQuestionSwitch = () => {
+    setShowCorrect(false);
+    setCurrentIndex(currentIndex + 1)
   }
   
-  return gameDone ? (
-    <h1 className="text-2xl text-white font-bold">Your score is {score}</h1>
-  ) : questions.length > 0 ? (
+  return questions.length > 0 ? (
     <div className="container">
-      <Questionnaire  
-      data={questions[currentIndex]} 
-      handleAnswer={handleAnswer} />
+      {currentIndex >= questions.length ? (
+        <h1 className="text-2xl text-white font-bold">
+          Game Over! Your score is {score}.
+        </h1>
+      ) : (
+        <Questionnaire
+          handleQuestionSwitch={handleQuestionSwitch}
+          showCorrect={showCorrect}
+          data={questions[currentIndex]}
+          handleAnswer={handleAnswer}
+        />
+      )}
     </div>
   ) : (
     <h2 className="text-2xl text-white font-bold">Loading...</h2>
-  )
+  );
     
 }
 
--- src/components/Questionnaire.js
+++ src/components/Questionnaire.js
@@ -1,27 +1,47 @@
-import React from 'react';
+import React from "react";
 
-
-const Questionnaire = ({handleAnswer, data: {question, correct_answer, incorrect_answers} }) => {
-  
-
-  const shuffledAnswers = [correct_answer, ...incorrect_answers].sort(() => Math.random() - 0.5)
+const Questionnaire = ({
+  handleQuestionSwitch,
+  showCorrect,
+  handleAnswer,
+  data: { question, correct_answer, answers },
+}) => {
 
   return (
-    <div>
-    <div className="bg-white text-blue-700 p-10 rounded-lg shadow-md">
-      <h2 className="text-2xl">{question}</h2>
+    <div className="flex flex-col">
+      <div className="bg-white text-blue-700 p-10 rounded-lg shadow-md">
+        <h2 className="text-2xl">{question}</h2>
+      </div>
+      <div className="grid grid-cols-2 gap-6 mt-6">
+        {answers.map((answer, idx) => {
+          const bgColor = showCorrect
+            ? answer === correct_answer
+              ? "bg-green-500"
+              : "bg-red-500"
+            : "bg-white-500";
+
+          const textColor = showCorrect ? "text-white-500" : "text-blue-800";
+          return (
+            <button
+              key={idx}
+              className={`bg-white ${bgColor} ${textColor} p-4 text-blue-800 font-semibold rounded shadow`}
+              onClick={() => handleAnswer(answer)}
+            >
+              {answer}
+            </button>
+          );
+        })}
+      </div>
+      {showCorrect && (
+        <button
+          onClick={handleQuestionSwitch}
+          className={`bg-yellow-700 p-4 text-white-800 font-semibold rounded shadow mt-6`}
+        >
+          Next Question
+        </button>
+      )}
     </div>
-    <div className="grid grid-cols-2 gap-6 mt-6">
-        {shuffledAnswers.map((answer,idx) => (
-          <button key={idx} className='bg-white p-4 text-blue-800 font-semibold rounded shadow'
-          onClick={() => handleAnswer(answer)}>
-            {answer}
-          </button>
-        ))}
-    </div>
-  </div>
-  )
-  
+  );
 };
 
 export default Questionnaire;
